cb9ce1e3b6e7b0ba059a52c2f74a3b7df8ed97d1
Rob Gansevles <rgansevles@gmail.com>
2014-09-05 14:08:37 +0200

    wip

--- sablo-examples/target/sablo-examples/js/sample_app.js
+++ sablo-examples/target/sablo-examples/js/sample_app.js
@@ -1,8 +1,11 @@
-angular.module('sampleApp', ['mylabel']).config(function() {
-}).controller("SampleController", function($scope, $rootScope) {
+angular.module('sampleApp', ['sablo', 'mylabel']).config(function() {
+}).controller("SampleController", function($scope, $rootScope, $window, $sabloApplication) {
 	$scope.windowTitle = 'Sample Aplication';
 	$scope.currentPanel = 'forms/mainForm.html';
-}).controller( ).controller( )
+	
+	$window.alert('Connecting...');
+	$sabloApplication.connect();
+})
 .run(function($window) {
 	$window.alert('Sample Startup');
 });
